e53bfecefc6b1ef9b7d84eb7cab681b9ff2ab749
Utsav Choudhury <utsav.choudhury1@gmail.com>
2025-08-20 15:05:44 +0300

    fixes

--- Boolean_Algebra_and_Flags/Examples/Example_3.py
+++ Boolean_Algebra_and_Flags/Examples/Example_3.py
@@ -1,10 +1,18 @@
 
 
-if ((vacation := input('Enter whether James is on vacation (yes/no): ')) == 'yes') or ((day := int(input('Enter a day as a 1-based integer: '))) > 5):
-    result = 'True'
-else:
-    result = 'False'
+#if ((vacation := input('Enter whether James is on vacation (yes/no): ')) == 'yes') or ((day := int(input('Enter a day as a 1-based integer: '))) > 5):
+   # result = 'True'
+#else:
+ #   result = 'False'
 
-print(result)
+#print(result)
+
+
+#
+
+while not (1 <= (weekday := int(input('Please give a day of the week')) <= 7)):
+    print('Please provide a valid date')
+while (response := input('Enter yes/no') != 'yes'):
+    print('Please provide a proper response')
 
 
