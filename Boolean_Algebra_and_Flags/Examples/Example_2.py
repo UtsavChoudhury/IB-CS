3d9d624c6c266f4ff9d537c628316bed140f42b9
Utsav Choudhury <utsav.choudhury1@gmail.com>
2025-08-20 14:42:50 +0300

    Quick fix

--- Boolean_Algebra_and_Flags/Examples/Example_2.py
+++ Boolean_Algebra_and_Flags/Examples/Example_2.py
@@ -24,5 +24,8 @@ if ((year % 4 == 0 and year % 100 != 0) or (year % 4 == 0 and year % 100 == 0 an
 if ((year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)):
     leap = True
 
+#Simplified further
+
+leap = ((year % 4 == 0 and year % 100 != 0) or (year % 400 == 0))
 
 print(leap)
