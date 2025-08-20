12ba969db3b9c01f6e25cf2573eac3865cfd169c
Utsav Choudhury <utsav.choudhury1@gmail.com>
2025-08-20 15:46:53 +0300

    SOLVED A PROBLEM!!!!!

---
+++ Boolean_Algebra_and_Flags/Homework/Homework_9.py
@@ -0,0 +1,22 @@
+def check_perfect(n):
+    sum = 0
+    k = 1
+
+    while (k < n):
+        if (sum <= n):
+            if (n % k == 0):
+                sum += k
+            k += 1
+        else:
+            return 0
+
+    if (sum == k):
+        return 1
+    
+
+j = 1
+
+while (j < 10000):
+    if check_perfect(j):
+        print(j, end= ' ')
+    j += 1
