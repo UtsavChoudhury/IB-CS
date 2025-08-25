4b65ec02706c498bd274990803defc579ccbc302
Utsav Choudhury <utsav.choudhury1@gmail.com>
2025-08-25 14:03:39 +0300

    jarmo plsss

--- Lists/Examples/Example_2.py
+++ Lists/Examples/Example_2.py
@@ -1,7 +1,10 @@
 
 lst = []
 while (inp := int(input('Enter a nonnegative integer: '))) > -1:
-    lst.append(inp)
-    lst.sort() # Not that efficient
+    index = 0
+    while index < len(lst) and lst[index] < inp: # HANDLES THE CASES WHERE THE SIZE OF THE LIST IS 0 AND WHERE WE NEED TO PUT AT THE END
+        index += 1
+    lst.insert(index, inp)
+    
 
 print(lst)
